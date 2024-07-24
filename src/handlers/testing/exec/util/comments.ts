bcd4a22ea5aa5db64ee5ae8c92475d3ddc3517e8
Adam Nolte <ajnolte12@gmail.com>
2024-07-24 16:37:18 -0500

    test

--- src/handlers/testing/exec/util/comments.ts
+++ src/handlers/testing/exec/util/comments.ts
@@ -16,8 +16,8 @@ const MAX_COMMIT_MESSAGE_LENGTH = 50;
 
 // Header names for the evaluators + test cases "table"
 const EVALUATOR_HEADER_NAME = 'Evaluators';
-const TEST_CASES_HEADER_NAME = 'Test Cases';
-const EVALUATOR_SCORE_HEADER_NAME = 'Scores';
+const TEST_CASES_HEADER_NAME = 'Pass / Fail / Skip';
+const EVALUATOR_SCORE_HEADER_NAME = 'Min / Avg / Max';
 
 // The number of spaces between the evaluator name column
 // and the test case stats column
@@ -601,8 +601,8 @@ function makeEvaluatorStatsTable(args: { evaluations: Evaluation[] }): string {
 
     evaluatorStats[evaluatorId] = {
       // Consider N/A as passed to simplify
-      testCasesString: `${(passedCount + naCount).toLocaleString()} PASSED / ${failedCount.toLocaleString()} FAILED / ${skippedCount.toLocaleString()} SKIPPED`,
-      scoreString: `${minScore.toFixed(2)} MIN / ${averageScore.toFixed(2)} AVG / ${maxScore.toFixed(2)} MAX`,
+      testCasesString: `${(passedCount + naCount).toLocaleString()} / ${failedCount.toLocaleString()} / ${skippedCount.toLocaleString()}`,
+      scoreString: `${minScore.toFixed(2)} / ${averageScore.toFixed(2)} / ${maxScore.toFixed(2)}`,
     };
   }
 
