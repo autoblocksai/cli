58b4b52c8b092f09f5f23e98286afff8998f4faf
Adam Nolte <adam@autoblocks.ai>
2024-08-15 08:11:58 -0500

    Adam/epd 1178 use axios in cli and add retries (#126)

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,42 +1,40 @@
 import { AUTOBLOCKS_API_BASE_URL } from './constants';
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+import { Semaphore } from './semaphore';
 
-type HttpError = {
-  status: number;
-  data: unknown;
-};
+// Limit to 10 concurrent requests
+const requestSemaphore = new Semaphore(10);
+
+axiosRetry(axios, {
+  retries: 3,
+  retryDelay: axiosRetry.exponentialDelay,
+});
 
 export async function post<T>(args: {
   path: string;
   apiKey: string;
   body?: unknown;
 }): Promise<T> {
-  const resp = await fetch(`${AUTOBLOCKS_API_BASE_URL}${args.path}`, {
-    method: 'POST',
-    body: args.body ? JSON.stringify(args.body) : undefined,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${args.apiKey}`,
-    },
+  return await requestSemaphore.run(async () => {
+    try {
+      const resp = await axios.post<T>(
+        `${AUTOBLOCKS_API_BASE_URL}${args.path}`,
+        args.body,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${args.apiKey}`,
+          },
+          timeout: 30_000,
+        },
+      );
+      return resp.data;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        throw new Error(`Failed to POST ${args.path}: ${err.toJSON()}`);
+      }
+      throw err;
+    }
   });
-
-  let data;
-  try {
-    data = await resp.json();
-  } catch {
-    const text = await resp.text();
-    const err: HttpError = {
-      status: resp.status,
-      data: text,
-    };
-    throw new Error(JSON.stringify(err));
-  }
-
-  if (!resp.ok) {
-    const err: HttpError = {
-      status: resp.status,
-      data,
-    };
-    throw new Error(JSON.stringify(err));
-  }
-  return data;
 }
