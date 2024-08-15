62136ec944c0b99163bcb5bb40aa90f220019463
Adam Nolte <ajnolte12@gmail.com>
2024-08-14 22:17:40 -0500

    limit concurrent requests

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,6 +1,10 @@
 import { AUTOBLOCKS_API_BASE_URL } from './constants';
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
+import { Semaphore } from './semaphore';
+
+// Limit to 10 concurrent requests
+const requestSemaphore = new Semaphore(10);
 
 axiosRetry(axios, {
   retries: 3,
@@ -12,23 +16,25 @@ export async function post<T>(args: {
   apiKey: string;
   body?: unknown;
 }): Promise<T> {
-  try {
-    const resp = await axios.post<T>(
-      `${AUTOBLOCKS_API_BASE_URL}${args.path}`,
-      args.body,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${args.apiKey}`,
+  return await requestSemaphore.run(async () => {
+    try {
+      const resp = await axios.post<T>(
+        `${AUTOBLOCKS_API_BASE_URL}${args.path}`,
+        args.body,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${args.apiKey}`,
+          },
+          timeout: 30_000,
         },
-        timeout: 30_000,
-      },
-    );
-    return resp.data;
-  } catch (err) {
-    if (axios.isAxiosError(err)) {
-      throw new Error(`Failed to POST ${args.path}: ${err.toJSON()}`);
+      );
+      return resp.data;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        throw new Error(`Failed to POST ${args.path}: ${err.toJSON()}`);
+      }
+      throw err;
     }
-    throw err;
-  }
+  });
 }
