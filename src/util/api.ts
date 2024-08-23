6631c5cf1a57470fc092bdae1cb630df85ea9e6b
Adam Nolte <adam@autoblocks.ai>
2024-08-23 11:47:06 -0500

    Update error display (#131)

--- src/util/api.ts
+++ src/util/api.ts
@@ -17,24 +17,17 @@ export async function post<T>(args: {
   body?: unknown;
 }): Promise<T> {
   return await requestSemaphore.run(async () => {
-    try {
-      const resp = await axios.post<T>(
-        `${AUTOBLOCKS_API_BASE_URL}${args.path}`,
-        args.body,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${args.apiKey}`,
-          },
-          timeout: 30_000,
+    const resp = await axios.post<T>(
+      `${AUTOBLOCKS_API_BASE_URL}${args.path}`,
+      args.body,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${args.apiKey}`,
         },
-      );
-      return resp.data;
-    } catch (err) {
-      if (axios.isAxiosError(err)) {
-        throw new Error(`Failed to POST ${args.path}: ${err.toJSON()}`);
-      }
-      throw err;
-    }
+        timeout: 30_000,
+      },
+    );
+    return resp.data;
   });
 }
