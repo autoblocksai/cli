bbc98d7fb6c420bddf6012453b93d4eed56c7924
nenadnovovicqc <nenad.novovic@qcerris.com>
2024-09-13 11:02:57 +0200

    typo

--- src/util/types.ts
+++ src/util/types.ts
@@ -2,5 +2,3 @@ export type Handler = () => void;
 export type AsyncHandler = () => Promise<void>;
 export type HandlerOf<T> = (val: T) => void;
 export type AsyncHandlerOf<T> = (val: T) => Promise<void>;
-
-export 
